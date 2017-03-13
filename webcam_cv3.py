1146b454472c15539580a92f8ad104ad3933768b
Nick <nrviens@gmail.com>
2017-03-13 13:49:02 -0400

    Added two new cascades, added_lane and merge.  Replaced the python code with mine which detects multiple cascades

--- webcam_cv3.py
+++ webcam_cv3.py
@@ -1,9 +1,13 @@
 import cv2
 import sys
 
-cascPath = 'haarcascade_frontalface_default.xml'
-faceCascade = cv2.CascadeClassifier(cascPath)
-font = cv2.FONT_HERSHEY_PLAIN
+cascPath = 'merge_cascade.xml'
+cascPath2 = 'added_lane_cascade.xml'
+cascPath3 = 'pedestrianCrossing_cascade.xml'
+mergeCascade = cv2.CascadeClassifier(cascPath)
+addedLaneCascade = cv2.CascadeClassifier(cascPath2)
+pedestrianCascade = cv2.CascadeClassifier(cascPath3)
+
 video_capture = cv2.VideoCapture(0)
 
 while True:
@@ -12,7 +16,23 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
+    merges = mergeCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    addedLanes = addedLaneCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
+    
+    pedestrians = pedestrianCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
         minNeighbors=5,
@@ -21,10 +41,15 @@ while True:
     )
 
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in merges:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        
+    for (x, y, w, h) in addedLanes:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-	cv2.putText(frame,'Face', (x,y), font, 5, (0, 255, 0), 2)
 
+    for (x, y, w, h) in pedestrians:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        
     # Display the resulting frame
     cv2.imshow('Video', frame)
 
