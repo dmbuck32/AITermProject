25de4f5f577e94fd2ea733b757c3873ad0e3344d
Jordan Brack <jbrack14@gmail.com>
2017-03-26 16:51:39 -0400

    Added color detection within recognzed signs.

--- color_detection.py
+++ color_detection.py
@@ -2,7 +2,7 @@ import cv2
 import sys
 import numpy as np
 
-cascPath = 'cascade.xml'
+cascPath = 'Cascades/signal_ahead_cascade.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
 
 video_capture = cv2.VideoCapture(0)
@@ -11,29 +11,40 @@ while True:
     # Capture frame-by-frame
     ret, frame = video_capture.read()
 
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
-
-
-    # define range of blue color in HSV
+    
     yellow_lower = np.array([20,90,80])
     yellow_upper = np.array([90,250,200])
     
     blue_lower = np.array([120, 50, 50])
     blue_upper = np.array([170, 255, 230])
 
-    # Threshold the HSV image to get only blue colors
-    mask = cv2.inRange(hsv, blue_lower, blue_upper)
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags=cv2.CASCADE_SCALE_IMAGE
+    )
 
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        roi = hsv[y:y+h, x:x+w]
+        mask = cv2.inRange(roi, yellow_lower, yellow_upper)
+        res = cv2.bitwise_and(roi, roi, mask= mask)
 
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('res',res)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+    try:
+        mask
+    except NameError:
         break
+    else:
+        cv2.imshow('Mask', mask)
+        cv2.imshow('res',res)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
